49cb0495b8665f38984534aa07d4c9dc10409355
Ivan Novak <ivannovak92@yahoo.com>
2017-08-20 22:24:29 +0200

    Add files via upload

---
+++ predlosci/meni.php
@@ -0,0 +1,7 @@
+<div style="size: " class="menu" style="background-color: black">
+  <a href="pindex.php">Pocetna</a>
+  <a href="statistika.php">Statistika</a>
+  <a href="roba.php">Baza podaci</a>
+  <a style="alignment-baseline: right" href="../logout.php" class="alert button">Logout</a>
+			
+</div>
