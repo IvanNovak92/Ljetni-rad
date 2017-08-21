5374d97c4573840f90bc179719392853125c4d25
Ivan Novak <ivannovak92@yahoo.com>
2017-08-21 14:27:58 +0200

    Add files via upload

--- predlosci/meni.php
+++ predlosci/meni.php
@@ -2,6 +2,7 @@
   <a href="pindex.php">Pocetna</a>
   <a href="statistika.php">Statistika</a>
   <a href="roba.php">Baza podaci</a>
+  <a href="era.php">Era</a>
   <a style="alignment-baseline: right" href="../logout.php" class="alert button">Logout</a>
 			
 </div>
