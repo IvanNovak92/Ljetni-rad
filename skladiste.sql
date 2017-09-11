9b75cd67a1ddf4f94788100f7a15c3af8ec7f171
Ivan Novak <ivannovak92@yahoo.com>
2017-09-11 13:48:55 +0200

    Add files via upload

--- skladiste.sql
+++ skladiste.sql
@@ -2,18 +2,16 @@ drop database if exists skladiste;
 create database skladiste default character set utf8;
 use skladiste;
 
-create table skladiste(
-sifra int not null primary key auto_increment,
-ime varchar(50) not null,
-vlasnik varchar(50) not null,
-lokacija varchar(50) not null,
-djelatnik int not null
-
-);
-
 create table komora(
 sifra int not null primary key auto_increment,
 broj_komore char(5) not null,
+polje varchar(50)
+
+);
+
+create table komora_roba(
+sifra int not null primary key auto_increment,
+komora int not null,
 roba int not null
 );
 
@@ -23,22 +21,16 @@ vrsta_robe varchar(50) not null,
 datum_berbe datetime not null,
 datum_skladistenja datetime not null,
 vrsta_boxa varchar(50) not null,
-komad_boxa int(2) not null,
-tezina decimal(4,1) not null
+komad_boxa int(2) not null
+
 
 );
 
 create table roba_kooperant(
 sifra int not null primary key auto_increment,
 roba int not null,
-kooperant int not null
-);
-
-create table polje(
-sifra int not null primary key auto_increment,
-naziv varchar(50) not null,
-roba int not null
-
+kooperant int not null,
+kolicina decimal(4,1) not null
 );
 
 create table kooperant(
@@ -65,10 +57,9 @@ uloga		varchar(50) not null
 
 );
 
-alter table skladiste add foreign key (djelatnik) references djelatnik(sifra);
-alter table komora add foreign key (roba) references roba(sifra);
 
-alter table polje add foreign key (roba) references roba(sifra);
+alter table komora_roba add foreign key (roba) references roba(sifra);
+alter table komora_roba add foreign key (komora) references komora(sifra);
 
 
 alter table roba_kooperant add foreign key (roba) references roba(sifra);
@@ -84,32 +75,29 @@ insert into djelatnik(ime,prezime,funkcija)
 values	('Marko','Markic','skladistar'),
 		('Niko','Nikić','skladistar');
 
-#3
-insert into skladiste(ime,vlasnik,lokacija,djelatnik)
-values	('Bora','Nikola d.o.o','Knezevi Vinogradi',1),
-		('Bora','Nikola d.o.o','Suljoš',2);
 		
 #4
-insert into roba(vrsta_robe,datum_berbe,datum_skladistenja,vrsta_boxa,komad_boxa,tezina)
-values	('Jabuka Granny','2016.8.9.','2016.10.10','Box Belje','4','1000,5'),
-		('Jabuka Jonagold','2016.6.6.','2016.7.7','Box Belje','3','700,5');
+insert into roba(vrsta_robe,datum_berbe,datum_skladistenja,vrsta_boxa,komad_boxa)
+values	('Jabuka Granny','2016.8.9.','2016.10.10','Box Belje','4'),
+		('Jabuka Jonagold','2016.6.6.','2016.7.7','Box Belje','3');
 
-#5
-insert into polje(naziv,roba)
-values	('A10',1),
-		('D1',2);
+
 		
 #6 
-insert into komora(broj_komore,roba)
-values	('10',1),
-		('1',2);
+insert into komora(broj_komore,polje)
+values	('10','A10'),
+		('1','B51');
 		
 #7
 insert into kooperant(ime,ziro_racun)
 values	('Belje','11111111111'),
 		('Tomislav Tomić','22222222222');
 		
-		insert into roba_kooperant(roba,kooperant)
+insert into roba_kooperant(roba,kooperant,kolicina)
+values	(1,1,'1000'),
+		(2,2,'2000');
+		
+		insert into komora_roba(komora,roba)
 values	(1,1),
 		(2,2);
 		
