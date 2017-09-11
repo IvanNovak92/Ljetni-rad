ace050d19e45d102c6438b7cdf3653863329d72a
Ivan Novak <ivannovak92@yahoo.com>
2017-09-11 13:54:15 +0200

    Add files via upload

--- predlosci/meni.php
+++ predlosci/meni.php
@@ -1,8 +1,9 @@
 <div style="size: " class="menu" style="background-color: black">
   <a href="<?php echo $putanjaAPP;  ?>privatno/roba/pindex.php">Pocetna</a>
   <a href="<?php echo $putanjaAPP;  ?>privatno/roba/roba.php">Dokumentacija robe</a>
-  <a href="<?php echo $putanjaAPP;  ?>privatno/kooperanti/kooperantIndex.php">Kooperanti</a>
-  <a href="<?php echo $putanjaAPP;  ?>privatno/era.php">Era</a>
+  <a href="<?php echo $putanjaAPP;  ?>privatno/kooperant/index.php">Kooperanti</a>
+  <a href="<?php echo $putanjaAPP;  ?>privatno/komora/index.php">Komore</a>
+  <a href="<?php echo $putanjaAPP;  ?>Era.png">Era</a>
   <a style="alignment-baseline: right" href="<?php echo $putanjaAPP;  ?>logout.php" class="alert button">Logout</a>
 			
 </div>
